b3faeaa2d44477d33fcf4266d3ce983e03446f12
ordinaryDamian <94764927+ordinaryDamian@users.noreply.github.com>
2024-02-25 00:06:29 +0100

    update scriptov

--- Install/plymouth.sh
+++ Install/plymouth.sh
@@ -1,16 +1,17 @@
 #! /bin/sh
 
-yay -S plymouth-theme-NAME-git
+yay -S plymouth-theme-cuts-alt-git 
 # packages needed - plymouth, plymouth-x11, plymouth-plugin-script(fedora)
 
 # after downloading or cloning themes, copy the selected theme in plymouth theme dir
-sudo cp -r NAME /usr/share/plymouth/themes/
+sudo cp -r cuts_alt /usr/share/plymouth/themes/
 
 # check if theme exist in dir
 sudo plymouth-set-default-theme -l
 
 # now set the theme (angular, in this case) and rebuilt the initrd
-sudo plymouth-set-default-theme -R NAME
+sudo plymouth-set-default-theme -R cuts_alt
 
 # optionally you can test theme by running the script given in repo (plymouth-x11 required)
 # sudo ./showplymouth.sh 20
+# https://www.debugpoint.com/install-plymouth-arch-linux/
