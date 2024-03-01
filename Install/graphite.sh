f076e110e4a463a833b3fff9f844db1ac91801e9
ordinaryDamian <94764927+ordinaryDamian@users.noreply.github.com>
2024-03-01 18:53:05 +0100

    Update graphite.sh

--- Install/graphite.sh
+++ Install/graphite.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 
-sudo pacman -S gnome-themes-extra -y
-sudo pacman -S gtk-engine-murrine -y
-sudo pacman -S sassc -y 
+sudo pacman -S gnome-themes-extra --noconfirm
+sudo pacman -S gtk-engine-murrine --noconfirm
+sudo pacman -S sassc --noconfirm
 
 git clone https://github.com/vinceliuice/Tela-circle-icon-theme
 cd Tela-circle-icon-theme
