95fdc5266d580734f0fef5c5dd65710ef0ce3394
ordinaryDamian <94764927+ordinaryDamian@users.noreply.github.com>
2024-03-01 18:01:14 +0100

    update

--- Install/graphite.sh
+++ Install/graphite.sh
@@ -6,12 +6,16 @@ sudo pacman -S sassc -y
 
 git clone https://github.com/vinceliuice/Tela-circle-icon-theme
 cd Tela-circle-icon-theme
-chmod +x install.sh
 ./install.sh
 
 cd ..
 
 git clone https://github.com/vinceliuice/Graphite-gtk-theme
 cd Graphite-gtk-theme/
-chmod +x install.sh
+./install.sh
+
+cd ..
+
+git clone https://github.com/vinceliuice/Graphite-cursors
+cd Graphite-cursors
 ./install.sh
--- Install/windowInstaler.sh
+++ Install/windowInstaler.sh
@@ -1,35 +1,36 @@
 #! /bin/sh
 
-sudo pacman -Syyu
+sudo pacman -Syyu --noconfirm
 
-sudo pacman -S xorg-server xorg-apps xf86-video-vmware  
+sudo pacman -S xorg-server xorg-apps xf86-video-vmware --noconfirm
 
-sudo pacman -S nemo reflector neofetch htop btop cmatrix 
+sudo pacman -S nemo reflector neofetch btop cmatrix man tldr cowsay --noconfirm
 
-#sudo pacman -S tty-clock -y
-#sudo pacman -S xsetroot -y
-#sudo pacman -S xrandr -y
-sudo pacman -S mlocate thunderbird network-manager-applet eza wget tilix 
+#sudo pacman -S tty-clock 
+#sudo pacman -S xsetroot 
+#sudo pacman -S xrandr 
+sudo pacman -S curl zip unzip mlocate thunderbird network-manager-applet eza wget tilix --noconfirm
 
 # https://github.com/adi1090x/rofi
-sudo pacman -S rofi -y 
+sudo pacman -S rofi --noconfirm
 git clone --depth=1 https://github.com/adi1090x/rofi.git
 cd rofi
 chmod +x setup.sh
 ./setup.sh
 
-sudo pacman -S fastfetch -y
-sudo pacman -S starship -y
+sudo pacman -S fastfetch --noconfirm
+sudo pacman -S starship --noconfirm
 
-sudo pacman -S dunst net-tools flameshot picom tldr trash-cli unzip lxappearance tar plymouth 
+sudo pacman -S dunst net-tools flameshot picom tldr trash-cli unzip lxappearance tar plymouth wezterm plymouth-x11 kate --noconfirm
 
-yay -S themechanger-git
+yay -S themechanger-git --noconfirm
 
 # https://aur.archlinux.org/packages/polybar-scripts-git
-sudo pacman -S ly -y
-# sudo systemctl enable ly.service
 
-sudo pacman -S feh dmenu kate polybar bspwm sxhkd wezterm xorg-xman hwinfo firefox python cantarell-fonts neovim mpv 
+sudo pacman -S ly --noconfirm
+sudo systemctl enable ly.service
+
+sudo pacman -S feh dmenu kate polybar bspwm sxhkd wezterm xorg-xman hwinfo firefox python cantarell-fonts neovim mpv --noconfirm
 
 sudo updatedb
 mkdir -p /home/$USER/.config/{bspwm,sxhkd,dunst,polybar,picom}
